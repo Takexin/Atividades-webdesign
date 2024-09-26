5f84fc77299178e476b7a55b1363020513148c78
Felipe Takeshi <167320163+Takexin@users.noreply.github.com>
2024-09-26 12:18:55 -0300

    teste

--- desafio__ajuste_site/script.js
+++ desafio__ajuste_site/script.js
@@ -2,19 +2,29 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
 var path = require('path');
+var gemini = require("@google/generative-ai");
+var ejs = require("ejs");
 
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+var data[{
+    prompt: "value"
+},];
+// app.use("/static",express.static(path.join(__dirname, "public")));
+app.get("/", (req, res)=>{
+    console.log("just got a get request");
+    res.render("pages/index");    
+});
+app.post("/prompt", (req, res)=>{
+    console.log("apertou o botao para prompt");
+
+    res.render("pages/prompt", {});
+    res.test = "omaga";
+    console.log(`res: ${req.body.prompt}`);
+});
 
 app.listen(3000, () =>{
     console.log("listening on port 3000");
 })
-app.use(bodyParser.urlencoded({extended: true}));
-app.use("/static",express.static(path.join(__dirname, "public")));
-app.get("/", (req, res)=>{
-    console.log("just got a get request")
-    res.send("test");
-});
-app.post("/static/index.html", (res, req)=>{
-    console.log("apertou o botao para prompt");
-    console.log(`res: ${res.body.prompt}`);
-});
-

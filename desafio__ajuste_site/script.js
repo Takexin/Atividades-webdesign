6fd43942984edca66d7d67031219fa40753f45e2
Felipe Takeshi <ftrangel07@gmail.com>
2024-09-26 21:43:08 -0300

    gemni integration

--- desafio__ajuste_site/script.js
+++ desafio__ajuste_site/script.js
@@ -2,27 +2,36 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
 var path = require('path');
-var gemini = require("@google/generative-ai");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 var ejs = require("ejs");
 
+const genAI = new GoogleGenerativeAI("AIzaSyBnlotxyzPa4MuJFfYg7UNIavqQcAGKzgE");
 
+
+async function chat(prompt, res) {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const result = await model.generateContent(prompt);
+    const message = await result.response.text();
+    return message;
+}
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
-var data[{
-    prompt: "value"
-},];
-// app.use("/static",express.static(path.join(__dirname, "public")));
+
+//app.use(express.static("views/pages"));
 app.get("/", (req, res)=>{
     console.log("just got a get request");
     res.render("pages/index");    
 });
-app.post("/prompt", (req, res)=>{
+app.post("/prompt", async(req, res)=>{
     console.log("apertou o botao para prompt");
-
-    res.render("pages/prompt", {});
-    res.test = "omaga";
+    var prompt = new String(req.body.prompt);
+    message =  await chat(prompt.toString());
+    res.send(message);
     console.log(`res: ${req.body.prompt}`);
+    console.log(message);
+
+
 });
 
 app.listen(3000, () =>{

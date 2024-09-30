85754aef9f0b75d31e8a6a648f29ff896d0834a3
Felipe Takeshi <167320163+Takexin@users.noreply.github.com>
2024-09-30 13:40:48 -0300

    going

--- desafio__ajuste_site/script.js
+++ desafio__ajuste_site/script.js
@@ -10,7 +10,7 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 
 async function chat(prompt, res) {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", systemInstruction: "Você irá receber 4 números inteiros, sendo eles respostas para as seguintes questões, de 1 a 5, 1 sendo discordo competamente e 5 concordo completamente:  Questão 1 'Avalie seu bem estar geral nesta semana', Questão 2 'Qual o seu grau de satisfação com o ambiente de trabalho?', Questão 3 'Você sente que seu trabalho possúi um impacto positivo nessa empresa?', Questão 4 'Você possúi as ferramentas e recursos necessários para realizar seu trabalho efetivamente?'. Faça uma análise de possíveis soluções para os problemas listados, comunique-se APENAS com elementos de lista HTML, seja breve e escreva apenas UMA frase por solução." });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", systemInstruction: res });
     const result = await model.generateContent(prompt);
     const message = await result.response.text();
     return message;
@@ -22,13 +22,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use("/img", express.static("views/pages/img"));
 app.use("/scripts", express.static("views/pages/scripts"))
 app.get("/", (req, res)=>{
-    console.log(process.env.API_KEY);
     console.log("just got a get request");
     res.render("pages/index");
 });
 app.post("/prompt", async(req, res)=>{
     var prompt = new String(req.body.prompt);
-    message =  await chat(prompt.toString());
+    sysIns = "Answer the prompt sent to you, but comunicate ONLY with HTML and CSS elements."
+    message =  await chat(prompt.toString(), sysIns);
     await res.render("pages/index", {text: message});
     console.log(`res: ${req.body.prompt}`);
     console.log(message);
@@ -41,11 +41,12 @@ app.post("/form", async(req, res)=>{
     var question4 = req.body.q4;
     var questionArray = [question1, question2, question3, question4];
     console.log(req.body);
-    var message = await chat(questionArray.toString());
+    sysIns = "Você irá receber 4 números inteiros, sendo eles respostas para as seguintes questões, de 1 a 5, 1 sendo discordo competamente e 5 concordo completamente:  Questão 1 'Avalie seu bem estar geral nesta semana', Questão 2 'Qual o seu grau de satisfação com o ambiente de trabalho?', Questão 3 'Você sente que seu trabalho possúi um impacto positivo nessa empresa?', Questão 4 'Você possúi as ferramentas e recursos necessários para realizar seu trabalho efetivamente?'. Faça uma análise de possíveis soluções para os problemas listados, comunique-se APENAS com elementos de lista HTML, seja breve e escreva apenas UMA frase por solução.";
+    var message = await chat(questionArray.toString(), sysIns);
     await res.render("pages/index", {formRes: message});
 });
 app.get("/test", (req, res) =>{
-    res.redirect("/");
+    res.redirect("/"); 
 });
 app.listen(3000, () =>{
     console.log("listening on port 3000");
